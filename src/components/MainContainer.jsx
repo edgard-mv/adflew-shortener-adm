8929c970c8f80b6e11f2dfbf4dac1a667a338f03
Edgard Morales <edgardmv04@gmail.com>
2022-06-26 18:18:59 -0600

    update background color

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,6 +1,7 @@
 import { styled } from '@mui/material/styles';
 
-const MainContainer = styled('main')(() => ({
+const MainContainer = styled('main')(({ theme }) => ({
+  backgroundColor: theme.palette.background.default,
   display: 'flex',
   flex: '1 1 auto',
   alignContent: 'center',
